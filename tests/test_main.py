263d4025777d6e6075bfea98f9dc6e3396c71744
Atindra11Mishra <atin.mishra11@gmail.com>
2025-07-03 02:14:39 +0530

    c

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
 from app.main import greet
 
 def test_greet():
-    assert greet("World") == "Hello, the Wow Amazon Headwuater World!"
+    assert greet("World") == "Hello, the Wow Amazon wow Headwuater World!"
